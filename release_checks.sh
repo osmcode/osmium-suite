70e5e5d8a952ff61202dde3facca304ec3d225d6
Jochen Topf <jochen@topf.org>
2023-09-20 14:34:13 +0200

    Update release checks
    * Check FindLZ4 is current
    * Check osmium-surplus instead of osm-data-model-tools repo

--- release_checks.sh
+++ release_checks.sh
@@ -6,7 +6,7 @@
 #
 #
 
-REPOSITORIES="osmium-tool osmium-contrib osm-gis-export osmcoastline osm-area-tools osmium-filter osm-data-model-tools"
+REPOSITORIES="osmium-tool osmium-contrib osm-gis-export osmcoastline osm-area-tools osmium-filter osmium-surplus"
 CATCH_REPOSITORIES="osmium-tool osmium-filter"
 ALL_REPOSITORIES="libosmium $REPOSITORIES"
 
@@ -23,7 +23,7 @@ RED="\033[1;31m"
 
 echo "== Comparing cmake files..."
 for repos in $REPOSITORIES; do
-    for file in FindOsmium FindProtozero; do
+    for file in FindLZ4 FindOsmium FindProtozero; do
         if diff -u libosmium/cmake/${file}.cmake $repos/cmake/${file}.cmake; then
             echo "[${GREEN}OK ${NORM}] ${file}"
         else
